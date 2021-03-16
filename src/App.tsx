c6f1e9b13d3bacb2f1b15e347a298e59107b4290
Daniel Alves <daniel.alves@skytef.intranet>
2021-03-15 21:25:05 -0300

    Componentização

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { GlobalStyle } from './styles/global';
 
+import { Header } from './components/Header'
+import { Dashboard } from './components/Dashboard'
+
 export function App() {
   return (
-    <div className="App">
-    <GlobalStyle />
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+     
+      <GlobalStyle />
+    </>
   );
 }
 
